ccb246f2944c94aab769f92412855f9a9b54e876
Promit Biswas <promit.biswas0710@gmail.com>
2024-10-21 11:05:42 +0530

    Initial Commit ... Header created

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src="https://www.freeiconspng.com/thumbs/linkedin-logo-png/linkedin-logo-3.png"
+          alt=""
+        />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+
+      <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
